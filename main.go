83b40697eadfe547092ca07e3eaaea5b17b59535
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2022-12-13 13:01:50 +0000

    feat: add main logic

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/AgileCraftsmanshipCanarias/kata-setup-go/kata"
+)
+
+func main() {
+	dependency := kata.NewDependencyImpl()
+	k := kata.New(dependency)
+
+	fmt.Printf("the value is %d", k.Value())
 }
